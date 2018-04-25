c176485271affd2a7e4f6e0e8899fe76ecbc3263
scarletz <ultimatescarlet@gmail.com>
2018-04-25 04:59:34 +0000

    getGeotest

---
+++ python/getGeo.py
@@ -0,0 +1,35 @@
+:^q^qoding: utf-8
+import sys
+from pygeocoder import Geocoder
+import urllib
+import os
+
+#args 場所（日本語）,ファイル名
+args=sys.argv
+address = args[1]
+results = Geocoder.geocode(address)
+print(results[0].coordinates)
+
+
+def download_pic(url,filename):
+	img = urllib.urlopen(url)
+	localfile = open( "./" + str(filename) + ".png" , 'wb')
+	localfile.write(img.read())
+	img.close()
+	localfile.close()
+
+
+result = Geocoder.reverse_geocode(*results.coordinates, language="ja")
+print result
+key=os.getenv("GOOGLEAPIKEYGEO")
+html1 = "https://maps.googleapis.com/maps/api/staticmap?center="
+html2 = "&maptype=hybrid&size=640x480&sensor=false&zoom=18&markers="
+html3 = "&key="+key
+axis = str((results[0].coordinates)[0]) + "," + str((results[0].coordinates)[1])
+
+html = html1 + axis + html2 + axis + html3
+
+print html
+
+download_pic(html,args[2])
+
